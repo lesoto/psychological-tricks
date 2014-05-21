aafabd68f9aefc2b4aecda5a55e24cb0360b3c61
lesoto <lesoto@lesoto.com>
2014-05-21 14:17:03 -0400

    Minor fixes - 006

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
  
-app.use(express.logger('dev'));
+var logger = require('morgan');
+app.use(logger); 
+
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
